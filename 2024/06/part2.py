e98182d6376af76692e4f7c8fcd2b9dca6c91504
Nick Barton <48698122+nickbarton-wiq@users.noreply.github.com>
2024-12-07 00:47:44 +1100

    day 6 refactor

--- 2024/06/part2.py
+++ 2024/06/part2.py
@@ -8,113 +8,108 @@ def parse_file(filename):
     return process_file(lines)
 
 
-class Positions:
-    def __init__(self, length, width, starting_pos=None):
-        self.length = length
-        self.width = width
-        self.obstacles = set()
-        self.starting_pos = starting_pos
-
-
 def process_file(lines):
 
-    positions = Positions(
+    map = Map(
         length=len(lines),
         width=len(lines[0]),
     )
     for x, line in enumerate(lines):
         for y, indicator in enumerate(line):
             if indicator == "#":
-                positions.obstacles.add((x, y))
+                map.obstacles.add((x, y))
             elif indicator == '^':
-                positions.starting_pos = (x, y)
-    return positions
+                map.starting_pos = (x, y)
+    return map
 
 
-def up(x, y):
-    return (x - 1, y)
+class Map:
+    def __init__(self, length, width, starting_pos=None):
+        self.length = length
+        self.width = width
+        self.obstacles = set()
+        self.starting_pos = starting_pos
+
+    def on_map(self, pos):
+        return pos[0] >= 0 and pos[0] < self.length and pos[1] >= 0 and pos[1] < self.width
 
 
-def down(x, y):
-    return (x + 1, y)
+class Guard:
+    def __init__(self, position, map):
+        self.position = position
+        self.map = map
+        self.direction = self.up
+        self.path = {self.position}
+        self.visited = {(self.position, self.direction)}
 
+    def up(self):
+        return (self.position[0] - 1, self.position[1])
 
-def left(x, y):
-    return (x, y - 1)
+    def down(self):
+        return (self.position[0] + 1, self.position[1])
 
+    def left(self):
+        return (self.position[0], self.position[1] - 1)
 
-def right(x, y):
-    return (x, y + 1)
+    def right(self):
+        return (self.position[0], self.position[1] + 1)
 
+    def turn_right(self):
+        if self.direction == self.up:
+            return self.right
+        elif self.direction == self.right:
+            return self.down
+        elif self.direction == self.down:
+            return self.left
+        elif self.direction == self.left:
+            return self.up
 
-def turn_right(current_dir):
-    if current_dir == up:
-        return right
-    elif current_dir == right:
-        return down
-    elif current_dir == down:
-        return left
-    elif current_dir == left:
-        return up
-
-
-def on_map(pos, length, width):
-    return pos[0] >= 0 and pos[0] < length and pos[1] >= 0 and pos[1] < width
-
-
-def generate_path(positions):
-    path = set()
-    current_dir = up
-    pos = positions.starting_pos
-    path.add(pos)
-    while True:
-        next_pos = current_dir(*pos)  # type: ignore
-        if next_pos in positions.obstacles:
-            current_dir = turn_right(current_dir)
+    def move_on_map(self):
+        next_pos = self.direction()  # type: ignore
+        if next_pos in self.map.obstacles:
+            self.direction = self.turn_right()
         else:
-            pos = next_pos
-        if not on_map(pos, positions.length, positions.width):
+            self.position = next_pos
+            self.path.add(self.position)
+        return self.map.on_map(self.position)
+
+    def add_to_visited(self):
+        self.visited.add((self.position, self.direction))  # type: ignore
+
+    def entering_loop(self):
+        return (self.position, self.direction) in self.visited
+
+
+def generate_obstacles(map):
+    guard = Guard(map.starting_pos, map)
+    while True:
+        if not guard.move_on_map():
             break
-        path.add(pos)
-    path.remove(positions.starting_pos)
-    print("total obstacle options:", len(path))
-    return path
+    guard.path.remove(map.starting_pos)
+    print("total obstacle options:", len(guard.path))
+    return guard.path
 
 
-def process_obstacle(args):
-    positions, new_obstacle = args
-    positions.obstacles.add(new_obstacle)
-    visited = set()
-    current_dir = up
-    pos = positions.starting_pos
-    visited.add((pos, current_dir))
-
+def move_guard(args):
+    map, new_obstacle = args
+    map.obstacles.add(new_obstacle)
+    guard = Guard(map.starting_pos, map)
     while True:
-        next_pos = current_dir(*pos)  # type: ignore
-        if next_pos in positions.obstacles:
-            current_dir = turn_right(current_dir)
-        else:
-            pos = next_pos
-
-        if not on_map(pos, positions.length, positions.width):
+        if not guard.move_on_map():
             return False
-        elif (pos, current_dir) in visited:
+        elif guard.entering_loop():
             return True
         else:
-            visited.add((pos, current_dir))
+            guard.add_to_visited()
 
 
 def main():
-    # positions = parse_file("2024/06/example.txt")
-    positions = parse_file("2024/06/input.txt")
-
-    path = generate_path(deepcopy(positions))
+    # map = parse_file("2024/06/example.txt")
+    map = parse_file("2024/06/input.txt")
     with Pool() as pool:
-        args = [(deepcopy(positions), obstacle) for obstacle in path]
-        results = pool.map(process_obstacle, args)
-
-    possible_obstacles_positions = sum(1 for result in results if result)
-    return f"{possible_obstacles_positions=}"
+        args = [(deepcopy(map), obstacle) for obstacle in generate_obstacles(deepcopy(map))]
+        results = pool.map(move_guard, args)
+    return f"{sum(1 for result in results if result)}"
 
 
 if __name__ == '__main__':
